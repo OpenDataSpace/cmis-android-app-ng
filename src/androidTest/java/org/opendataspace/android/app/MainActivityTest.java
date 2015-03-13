ae3e44f6b2ba7f1ea9585fc1667d3f37c494bd51
Paul Kolomiets <paul.kolomiets@gmail.com>
2015-03-13 10:06:49 +0300

    initial commit

---
+++ src/androidTest/java/org/opendataspace/android/app/MainActivityTest.java
@@ -0,0 +1,11 @@
+package org.opendataspace.android.app;
+
+import android.test.ActivityInstrumentationTestCase2;
+import org.opendataspace.android.app.ui.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+}
