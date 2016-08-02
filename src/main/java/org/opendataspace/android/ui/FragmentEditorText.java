f4fba3c3fc50e8cc0aea46e5801d709ce8baa216
Paul Kolomiets <paul.kolomiets@gmail.com>
2016-08-02 07:16:45 +0300

    prompt save for text editor

--- src/main/java/org/opendataspace/android/ui/FragmentEditorText.java
+++ src/main/java/org/opendataspace/android/ui/FragmentEditorText.java
@@ -2,25 +2,33 @@ package org.opendataspace.android.ui;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import org.apache.chemistry.opencmis.commons.impl.IOUtils;
+import org.opendataspace.android.app.CompatDeprecated;
 import org.opendataspace.android.app.OdsLog;
 import org.opendataspace.android.app.TaskOperation;
 import org.opendataspace.android.app.WeakCallback;
 import org.opendataspace.android.app.beta.R;
 import org.opendataspace.android.operation.OperationLoadText;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 @SuppressLint("ValidFragment")
 public class FragmentEditorText extends FragmentBase {
@@ -29,6 +37,7 @@ public class FragmentEditorText extends FragmentBase {
 
     private final OperationLoadText op;
     private boolean hasSpeech;
+    private boolean hasChanges;
 
     public FragmentEditorText(final OperationLoadText op) {
         this.op = op;
@@ -44,9 +53,25 @@ public class FragmentEditorText extends FragmentBase {
     public void onActivityCreated(final Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        this.<ScrollView>widget(R.id.editor_view_scroll).setSmoothScrollingEnabled(true);
+        this.<EditText>widget(R.id.editor_edit_main).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(final CharSequence charSequence, final int i, final int i1, final int i2) {
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence charSequence, final int i, final int i1, final int i2) {
+                hasChanges = true;
+            }
+
+            @Override
+            public void afterTextChanged(final Editable editable) {
+            }
+        });
+
         final Activity ac = getActivity();
         hasSpeech = !ac.getPackageManager().queryIntentActivities(createSpeechIntent(), 0).isEmpty();
-        new TaskOperation<>(op, new WeakCallback<>(this, FragmentEditorText::textAvailable)).start();
+        actionReload();
     }
 
     @Override
@@ -79,6 +104,10 @@ public class FragmentEditorText extends FragmentBase {
             actionSpeech();
             break;
 
+        case R.id.menu_editor_reload:
+            actionReload();
+            break;
+
         default:
             return super.onOptionsItemSelected(item);
         }
@@ -87,6 +116,21 @@ public class FragmentEditorText extends FragmentBase {
     }
 
     private void actionSave() {
+        if (op.getFile() == null) {
+            return;
+        }
+
+        OutputStream sourceFile = null;
+
+        try {
+            sourceFile = new FileOutputStream(op.getFile());
+            sourceFile.write(this.<EditText>widget(R.id.editor_edit_main).getText().toString().getBytes("UTF-8"));
+            hasChanges = false;
+        } catch (Exception ex) {
+            OdsLog.ex(getClass(), ex);
+        } finally {
+            IOUtils.closeQuietly(sourceFile);
+        }
     }
 
     private void actionSpeech() {
@@ -122,9 +166,47 @@ public class FragmentEditorText extends FragmentBase {
     }
 
     private void textAvailable(final OperationLoadText op) {
+        final ActivityBase ac = (ActivityBase) getActivity();
+        ac.stopWait();
+
+        if (op.reportError(ac)) {
+            return;
+        }
+
         this.<EditText>widget(R.id.editor_edit_main).setText(op.getText());
+        hasChanges = false;
         getNavigation().updateTitle();
     }
 
+    @Override
+    public boolean backPressed() {
+        if (hasChanges && op.getFile() != null) {
+            requestSave();
+            return true;
+        }
 
+        return false;
+    }
+
+    private void requestSave() {
+        final Activity ac = getActivity();
+
+        new AlertDialog.Builder(ac).setTitle(R.string.editor_modified)
+                .setMessage(CompatDeprecated.fromHtml(getString(R.string.editor_quit)))
+                .setPositiveButton(R.string.editor_save, (dialog, item) -> {
+                    dialog.dismiss();
+                    actionSave();
+                    ac.finish();
+                }).
+                setNegativeButton(R.string.editor_discard, (dialog, item) -> {
+                    dialog.dismiss();
+                    ac.finish();
+                }).setNeutralButton(R.string.common_cancel, (dialog, item) -> dialog.dismiss()).create().show();
+    }
+
+    private void actionReload() {
+        final ActivityBase ac = (ActivityBase) getActivity();
+        ac.startWaitDialog(getTile(ac), getString(R.string.common_pleasewait), di -> op.setCancel(true));
+        new TaskOperation<>(op, new WeakCallback<>(this, FragmentEditorText::textAvailable)).start();
+    }
 }
